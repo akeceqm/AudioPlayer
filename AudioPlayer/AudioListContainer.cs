af3324a5baa8f4dcbfad64228a0cdea73cb291d4
akeceqm <vladkonovalov20060@gmail.com>
2024-01-27 21:02:42 +0500

    Добавьте файлы проекта.

---
+++ AudioPlayer/AudioListContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioPlayer
+{
+    public  class AudioListContainer
+    {
+        public List<string> listMusic { get; set; }
+
+        public AudioListContainer()
+        {
+            listMusic = new List<string>();
+        }
+    }
+}
---
+++ AudioPlayer/ChangeTrack.cs
@@ -0,0 +1,65 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioPlayer
+{
+    partial class ChangeTrack:AudioListContainer
+    {
+        int currentSongIndex = 0;
+        private List<string> listMusic;
+        WaveOutEvent outputDevice = new WaveOutEvent();
+
+        public ChangeTrack(List<string> listMusic, WaveOutEvent outputDevice)
+        {
+            this.listMusic = listMusic;
+            this.outputDevice = outputDevice;
+            this.currentSongIndex = 0;
+        }
+
+
+
+        public void NextTrack()
+        {
+            if (listMusic.Count > 0)
+            {
+                outputDevice.Stop();
+                currentSongIndex = (currentSongIndex + 1) % listMusic.Count;
+
+                Console.WriteLine($"Переключено на следующую песню: {listMusic[currentSongIndex]}");
+
+                var newAudioReader = new AudioFileReader(listMusic[currentSongIndex]);
+
+                outputDevice.Init(newAudioReader);
+                outputDevice.Play();
+            }
+            else
+            {
+                Console.WriteLine("Отсутствуют аудиофайлы.");
+            }
+        }
+
+        public void BackTrack()
+        {
+            if (listMusic.Count > 0)
+            {
+                outputDevice.Stop();
+                currentSongIndex = (currentSongIndex - 1 + listMusic.Count) % listMusic.Count;
+
+                Console.WriteLine($"Переключено на предыдущую песню: {listMusic[currentSongIndex]}");
+
+                var newAudioReader = new AudioFileReader(listMusic[currentSongIndex]);
+
+                outputDevice.Init(newAudioReader);
+                outputDevice.Play();
+            }
+            else
+            {
+                Console.WriteLine("Отсутствуют аудиофайлы.");
+            }
+        }
+    }
+}
